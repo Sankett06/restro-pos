db27bb22d193dd72f6a73604c9283a5190f5c319
sankett06 <sanketbandodkar455@gmail.com>
2025-07-17 20:37:11 +0530

    Update CORS for deployed new

--- server/server.js
+++ server/server.js
@@ -24,6 +24,7 @@ const PORT = process.env.PORT || 3001;
 // CORS configuration
 const allowedOrigins = [
   'http://localhost:5173',
+  'https://restro-pos-1.onrender.com',
   'https://test3.codeblocks.in'
 ];
 
